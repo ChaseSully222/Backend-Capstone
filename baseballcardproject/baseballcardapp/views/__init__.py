34d1384449b97963b79c22eb4348077a0ff5f96d
ChaseSully222 <Jcsullins222@yahoo.com>
2020-06-17 16:45:13 -0500

    resolved db_cursor error

--- baseballcardproject/baseballcardapp/views/__init__.py
+++ baseballcardproject/baseballcardapp/views/__init__.py
@@ -1,3 +1,4 @@
 from baseballcardapp.views.auth.logout import logout_user
 from .cards.list import card_list
 from .home import home
+from .connection import Connection
--- baseballcardproject/baseballcardapp/views/cards/list.py
+++ baseballcardproject/baseballcardapp/views/cards/list.py
@@ -7,6 +7,8 @@ def card_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
             conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
             db_cursor.execute("""
             select
                 c.playerId,
