52167c75c3263e985f0a7b91c9e3ffb91803f3dc
ChaseSully222 <Jcsullins222@yahoo.com>
2020-06-22 15:22:41 -0500

    player details page now shows all cards in the db for that player

--- baseballcardproject/baseballcardapp/views/players/details.py
+++ baseballcardproject/baseballcardapp/views/players/details.py
@@ -23,14 +23,39 @@ def get_player(playerId):
 
         return db_cursor.fetchone()
 
+def get_player_cards(playerId):
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Card)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            c.id,
+            c.playerId_id,
+            c.year,
+            c.setId_id,
+            c.cardNumber,
+            c.imagePathFront,
+            c.imagePathBack,
+            c.attribute,
+            se.name name
+        FROM baseballcardapp_card c
+        JOIN baseballcardapp_set se ON c.setId_id = se.id
+        WHERE c.playerId_id = ?
+        """, (playerId,))
+
+        return db_cursor.fetchall()
+
 @login_required
 def player_details(request, playerId):
     if request.method == 'GET':
         player = get_player(playerId)
+        cards = get_player_cards(playerId)
 
         template = 'players/detail.html'
         context = {
-            'player': player
+            'player': player,
+            'cards': cards
         }
 
         return render(request, template, context)
